fc50e95a7af4d8e6edd945b6e71e0f006b80233a
Gabriel Frigo <gabriel.frigo4@gmail.com>
2026-03-03 22:42:37 -0300

    Update sensor.hpp

--- System/lib/sensor/src/sensor.hpp
+++ System/lib/sensor/src/sensor.hpp
@@ -5,18 +5,30 @@ namespace tt
 {
 	namespace sensor
 	{
+		enum sensor_mode_t : uint16_t
+		{
+			sender = 0,
+			receiver = 1,
+			none = 2
+		};
+
 		struct sensor_t
 		{
 			int left;
 			int front;
 			int right;
+			sensor_mode_t mode;
 		};
 
 		void setup();
+		void init();
 		sensor_t create_snapshot();
+		sensor_mode_t get_mode();
+		void set_mode(sensor_mode_t mode);
 		void debug(char *out_buffer, const size_t out_size, sensor_t sensor, const char *msg);
 		void debug(sensor_t sensor, const char *msg);
 	}
 
 	typedef sensor::sensor_t sensor_t;
+	typedef sensor::sensor_mode_t sensor_mode_t;
 }
